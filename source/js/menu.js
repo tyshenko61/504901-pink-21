f2823a344274ddd4cf61554cf47bd11ac6e9d139
tyshenko61 <34244584+tyshenko61@users.noreply.github.com>
2021-01-14 18:59:54 +0500

    исправление замечаний

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,12 +1,17 @@
-const navMain = document.querySelector('.page-header');
-const buttonToggle = document.querySelector('.page-header__toggle');
+const navMain = document.querySelector('.main-nav');
+const buttonToggle = document.querySelector('.main-nav__toggle');
+const pageHeader = document.querySelector('.page-header');
 
-navMain.classList.add('page-header--close');
+navMain.classList.add('main-nav--close');
+pageHeader.classList.add('page-header--close');
+buttonToggle.classList.remove('main-nav__toggle--nojs');
 
 buttonToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('page-header--close')) {
-    navMain.classList.remove('page-header--close');
+  if (navMain.classList.contains('main-nav--close')) {
+    navMain.classList.remove('main-nav--close');
+    pageHeader.classList.remove('page-header--close');
   } else {
-    navMain.classList.add('page-header--close');
+    navMain.classList.add('main-nav--close');
+    pageHeader.classList.add('page-header--close');
   }
 });
